7629aaefdd639f6e11ca2af27bc6c8ff0ff3c5c1
Paul Harter <paul@glowinthedark.co.uk>
2019-03-18 20:59:27 +0000

    changes to template

--- config_local_template.py
+++ config_local_template.py
@@ -1,24 +1,13 @@
 
-# the address of the target machine if it fails to find it from the hostname.local
-ACCESS_IP = None
-
-# the root password to set
-ROOT_PASSWORD = "xxxxxx"
+ACCESS_IP = "xx.xx.xx.xx"
 
 ORIGINAL_HOSTNAME = "raspberrypi"
 ORIGINAL_USERNAME = "pi"
 ORIGINAL_PASSWORD = "raspberry"
 
-# the new password for the default user
-CHANGED_PASSWORD = "xxxxxx"
-
-# the new user
-NEW_USERNAME = "xxxxxx"
+NEW_USERNAME = "ishiki"
 NEW_PASSWORD = "xxxxxx"
-
-# the temporary hostname
 NEW_HOSTNAME = "noname"
 
-# name of the rsa certificate to use from the certs folder for accessing this device in the future
-CERTS_NAME = "xxxxxx"
+CERTS_NAME = "ishiki_admin_id_rsa"
 
