29cf5f59ae2c36b6b57ec9eb413cfbdf002f4258
Francesco Anselmo <francesco.anselmo@gmail.com>
2021-08-12 00:41:00 +0100

    bootstrapping from /boot working

--- bootstrap/start-boot.sh
+++ bootstrap/start-boot.sh
@@ -14,10 +14,10 @@ fi
 
 if [ -f /boot/docker-compose.yaml ]; then
     echo "Ishiki /boot folder bootstrap: docker compose pull"
-    docker-compose -f /boot/docker-compose.yaml stop
-    docker-compose -f /boot/docker-compose.yaml pull
+    cd /boot ; docker-compose stop
+    cd /boot ; docker-compose pull
     echo "Ishiki /boot folder bootstrap: docker compose up"
-    docker-compose -f /boot/docker-compose.yaml up --force-recreate -d
+    cd /boot ; docker-compose up --force-recreate -d
     docker system prune --force
 fi
 
